9236910dafe0f6ac6f370bf3a49e6b718080d5a1
林易興-程式專用 <lintest7811183@gmail.com>
2019-06-19 09:05:16 +0800

    2019-06-19-PenVC

---
+++ FriendRecord/MainApp/PenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PenViewController.swift
+//  FriendRecord
+//
+//  Created by 林易興 on 2019/6/18.
+//  Copyright © 2019 林易興. All rights reserved.
+//
+
+import UIKit
+
+class PenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
