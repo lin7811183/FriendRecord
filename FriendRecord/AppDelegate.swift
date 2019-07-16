1f497aae7f1e3ad6aa02f71cf57512121e184f7b
林易興-程式專用 <lintest7811183@gmail.com>
2019-07-16 21:49:30 +0800

    2019-07-16-優化Ver.3

--- FriendRecord/AppDelegate.swift
+++ FriendRecord/AppDelegate.swift
@@ -60,17 +60,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
-        //print("Notification  ID = \(userInfo[""])")
-        print("userInfo : \(userInfo)")
-    }
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        print("Registration succeeded!")
-    }
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
-        print("Registration failed!")
-    }
+//    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+//        //print("Notification  ID = \(userInfo[""])")
+//        print("userInfo : \(userInfo)")
+//    }
+//    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        print("Registration succeeded!")
+//    }
+//    
+//    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: NSError) {
+//        print("Registration failed!")
+//    }
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
